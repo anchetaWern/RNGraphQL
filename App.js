dd6486a52412954488ee1274676273faa7cae071
Dihan <dihan.algama@gmail.com>
2020-01-30 14:55:28 +0000

    Fixed Errors
    Fix for:
    
     "Attempted to assign to readonly property."  by apollo-boost
    
    "Unknown argument type ‘attribute’ in method -[RCTAppState getCurrentAppState:error:]. Extend RCTConvert to support this type."  by React Native 0.59.5 and needs to up upgraed to .0.59.9+

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import React, { Component } from "react";
 import { ActivityIndicator } from "react-native";
 
 import { ApolloProvider, Query } from "react-apollo";
-import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
-
-const client = new ApolloClient({
-  uri: "http://192.168.1.73:4000/graphql"
-});
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 
 import Pokemon from "./src/components/Pokemon";
 import getRandomInt from "./src/helpers/getRandomInt";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://192.168.1.73:4000/graphql"
+});
+const client = new ApolloClient({
+  link,
+  cache
+});
 
 export const AppContext = React.createContext({ data: { pokemon: null } });
 
@@ -44,10 +51,17 @@ export default class App extends Component {
     `;
   };
 
+  onGetNewPokemon = () => {
+    const query = this.getQuery();
+    this.setState({
+      query
+    });
+  };
+
   render() {
     const { query } = this.state;
     if (!query) return null;
-    return <ActivityIndicator size="large" color="#0000ff" />;
+    // return <ActivityIndicator size="large" color="#0000ff" />;
     return (
       <ApolloProvider client={client}>
         <Query
@@ -72,13 +86,6 @@ export default class App extends Component {
     );
   }
   //
-
-  onGetNewPokemon = () => {
-    const query = this.getQuery();
-    this.setState({
-      query
-    });
-  };
 }
 //
 
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,48 +1,49 @@
-import React from 'react';
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
-import { AppContext } from '../../App';
+import React from "react";
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
+import { AppContext } from "../../App";
 
 const Pokemon = () => {
   return (
     <AppContext.Consumer>
-      {
-        ({ name, pic, types, desc, onPress }) =>
+      {({ name, pic, types, desc, onPress }) => (
         <TouchableOpacity onPress={onPress}>
           <View style={styles.mainDetails}>
             <Image
-              source={{uri: pic}}
-              style={styles.image} resizeMode={"contain"} />
-              <Text style={styles.mainText}>{name}</Text>
+              source={{ uri: pic }}
+              style={styles.image}
+              resizeMode={"contain"}
+            />
+            <Text style={styles.mainText}>{name}</Text>
 
-              <FlatList
-                columnWrapperStyle={styles.types}
-                data={types}
-                numColumns={2}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => {
-                  return (
-                    <View style={[styles[item.name], styles.type]}>
-                      <Text style={styles.typeText}>{item.name}</Text>
-                    </View>
-                  )
-                }}
-              />
+            <FlatList
+              columnWrapperStyle={styles.types}
+              data={types}
+              numColumns={2}
+              keyExtractor={item => item.id.toString()}
+              renderItem={({ item }) => {
+                return (
+                  <View style={[styles[item.name], styles.type]}>
+                    <Text style={styles.typeText}>{item.name}</Text>
+                  </View>
+                );
+              }}
+            />
 
-              <View style={styles.description}>
-                <Text>{desc}</Text>
-              </View>
+            <View style={styles.description}>
+              <Text>{desc}</Text>
+            </View>
           </View>
         </TouchableOpacity>
-      }
+      )}
     </AppContext.Consumer>
   );
-}
+};
 
 //
 const styles = {
   mainDetails: {
     padding: 30,
-    alignItems: 'center'
+    alignItems: "center"
   },
   image: {
     width: 100,
@@ -50,78 +51,78 @@ const styles = {
   },
   mainText: {
     fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "center"
   },
   description: {
     marginTop: 20
   },
   types: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 20
   },
   type: {
     padding: 5,
     width: 100,
-    alignItems: 'center'
+    alignItems: "center"
   },
   typeText: {
-    color: '#fff',
+    color: "#fff"
   },
   normal: {
-    backgroundColor: '#8a8a59'
+    backgroundColor: "#8a8a59"
   },
   fire: {
-    backgroundColor: '#f08030'
+    backgroundColor: "#f08030"
   },
   water: {
-    backgroundColor: '#6890f0'
+    backgroundColor: "#6890f0"
   },
   electric: {
-    backgroundColor: '#f8d030'
+    backgroundColor: "#f8d030"
   },
   grass: {
-    backgroundColor: '#78c850'
+    backgroundColor: "#78c850"
   },
   ice: {
-    backgroundColor: '#98d8d8'
+    backgroundColor: "#98d8d8"
   },
   fighting: {
-    backgroundColor: '#c03028'
+    backgroundColor: "#c03028"
   },
   poison: {
-    backgroundColor: '#a040a0'
+    backgroundColor: "#a040a0"
   },
   ground: {
-    backgroundColor: '#e0c068'
+    backgroundColor: "#e0c068"
   },
   flying: {
-    backgroundColor: '#a890f0'
+    backgroundColor: "#a890f0"
   },
   psychic: {
-    backgroundColor: '#f85888'
+    backgroundColor: "#f85888"
   },
   bug: {
-    backgroundColor: '#a8b820'
+    backgroundColor: "#a8b820"
   },
   rock: {
-    backgroundColor: '#b8a038'
+    backgroundColor: "#b8a038"
   },
   ghost: {
-    backgroundColor: '#705898'
+    backgroundColor: "#705898"
   },
   dragon: {
-    backgroundColor: '#7038f8'
+    backgroundColor: "#7038f8"
   },
   dark: {
-    backgroundColor: '#705848'
+    backgroundColor: "#705848"
   },
   steel: {
-    backgroundColor: '#b8b8d0'
+    backgroundColor: "#b8b8d0"
   },
   fairy: {
-    backgroundColor: '#e898e8'
+    backgroundColor: "#e898e8"
   }
-}
+};
 
 export default Pokemon;
